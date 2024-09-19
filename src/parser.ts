d889b5b74a6ca2a2e7bec86e2ad0648e2418fb31
Peter Holló <hello@songsforthe.dev>
2024-09-19 11:24:30 +0200

    feat: compile only html to html

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,7 @@
 import { readFile } from "node:fs/promises";
 import { config } from "./config";
 import handlebars from "handlebars";
+import dedent from "dedent";
 
 let layout: string | null = null;
 
@@ -16,32 +17,32 @@ export async function parse(
 
   const files = await Promise.all(
     path.map(async (path) => {
-      const baseTemplate = handlebars.compile(`
-          <!DOCTYPE html>
-          <html lang="en">
-          <head>
-              <meta charset="UTF-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <title>{{ title }}</title>
-          </head>
-              <body>
-                  {{{ content }}}
-              </body>
-          </html>`);
-
       const data = await readFile(path, "utf8");
       const template = handlebars.compile(data);
 
       const title = path.replace(config.files.baseDir, "").replace(".hbs", "");
 
       if (path.endsWith("text.hbs") || layout === null) {
-        const content = baseTemplate({ title, content: template({}) });
+        const content = template({});
         return { path, content };
       }
+      const htmlTemplate = handlebars.compile(dedent`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>{{ title }}</title>
+        </head>
+          <body>
+            {{{ content }}}
+          </body>
+        </html>
+      `);
 
       const layoutTemplate = handlebars.compile(layout);
       handlebars.registerPartial("body", template({}));
-      const content = baseTemplate({
+      const content = htmlTemplate({
         title,
         content: layoutTemplate({}),
       });
